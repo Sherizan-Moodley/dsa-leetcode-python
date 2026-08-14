4e0eb512ec8110aac64e3311734eca7f241e5123
Sherizan Moodley <sherizanmoodley12@gmail.com>
2026-08-14 17:42:19 +0200

    Add LeetCode 167 - Two Sum II

---
+++ arrays/math/arrays/two_pointers/two_sum_ii.py
@@ -0,0 +1,20 @@
+# LeetCode 167 - Two Sum II
+# Pattern: Two Pointers
+# Difficulty: Medium
+
+class Solution:
+    def twoSum(self, numbers, target):
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            total = numbers[left] + numbers[right]
+
+            if total == target:
+                return [left + 1, right + 1]
+
+            elif total < target:
+                left += 1
+
+            else:
+                right -= 1
