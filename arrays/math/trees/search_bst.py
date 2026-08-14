59a9a95f4a8f8cfe640f047a70f43cde687f3c37
Sherizan Moodley <sherizanmoodley12@gmail.com>
2026-08-14 17:44:53 +0200

    Add LeetCode 700 - Search in BST

---
+++ arrays/math/trees/search_bst.py
@@ -0,0 +1,16 @@
+# LeetCode 700 - Search in a Binary Search Tree
+# Pattern: Binary Search Tree
+# Difficulty: Easy
+
+class Solution:
+    def searchBST(self, root, val):
+        if root is None:
+            return None
+
+        if root.val == val:
+            return root
+
+        if val < root.val:
+            return self.searchBST(root.left, val)
+
+        return self.searchBST(root.right, val)
