e72893b86c3b0fc835e693d33acda249d833ed18
Sherizan Moodley <sherizanmoodley12@gmail.com>
2026-08-14 17:43:57 +0200

    Add LeetCode 704 - Binary Search

---
+++ arrays/math/arrays/two_pointers/searching/binary_search.py
@@ -0,0 +1,22 @@
+# LeetCode 704 - Binary Search
+# Pattern: Binary Search
+# Difficulty: Easy
+
+class Solution:
+    def search(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            elif nums[mid] < target:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        return -1
